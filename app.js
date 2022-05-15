86b37bf00df9d39173faa96a0829e1f064aeda9a
talitaburbulhan <talitaburbulhan91@gmail.com>
2022-05-15 00:57:57 -0300

    adiciona  js

---
+++ app.js
@@ -0,0 +1,26 @@
+function escutaRolagem(){
+
+    let gatilhos = document.querySelectorAll('.gatilhos > div');
+    
+    for(let gatilho of gatilhos){   
+
+        let posicao = gatilho.getBoundingClientRect();
+
+        let alvo = gatilho.dataset.alvo;
+        let passo = document.querySelector('.'+alvo);
+        
+        if(posicao.top <= 0 && posicao.top > -  posicao.height) {
+
+            passo.classList.add('passo-ativo');
+
+        }else{
+            passo.classList.remove('passo-ativo');       
+
+        }
+    
+
+    }
+
+}    
+
+window.addEventListener('scroll', escutaRolagem);
